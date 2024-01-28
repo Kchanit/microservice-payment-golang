7001feb40a40a1fd658b260a10051efd9c593793
ratchaphon1412 <ratchaphon.h111@gmail.com>
2024-01-28 17:46:24 +0700

    create producer kafka

---
+++ internal/core/utils/broker/producer.go
@@ -0,0 +1,55 @@
+package broker
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+)
+
+func KafkaProducer(topic string, content map[string]interface{}) error {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "kafka:9092"})
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	defer p.Close()
+
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
+	// Produce messages to topic (asynchronously)
+
+	// content := map[string]interface{}{
+	// 	"message": "Hello World!",
+	// }
+	value, err := json.Marshal(content)
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          value,
+	}, nil)
+
+	// Wait for message deliveries before shutting down
+	p.Flush(15 * 1000)
+
+	return nil
+}
